587bc631843e9c8e31cc516b23ca6d055fbcbcd4
Olga Borisenko <olka_borisenko@mail.ru>
2022-12-07 19:40:19 +0300

    Нашли вторую цифру в трехзначных числах

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число 1: ");
+int a = int.Parse(Console.ReadLine());
+
+int b = (a/10);
+int c = (b%10);
+
+{
+    Console.WriteLine($"Вторая цифра {c}");
+}
+
+
+
