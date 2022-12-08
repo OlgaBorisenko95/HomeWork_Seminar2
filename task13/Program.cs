bfba7676cb72dd6508cc0938ba268e3663ed1a41
Olga Borisenko <olka_borisenko@mail.ru>
2022-12-08 18:47:38 +0300

    Нашли третью цифру числа

---
+++ task13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число 1: ");
+int a = int.Parse(Console.ReadLine());
+
+int b = (a%10);
+if (a<100) 
+{
+    Console.WriteLine("Третьей цифры нет");  
+}
+else
+
+    Console.WriteLine($"Третья цифра {b}");
