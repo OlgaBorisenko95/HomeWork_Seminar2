270d57b0255258d6aa7062e42e387e9ce9157497
Olga Borisenko <olka_borisenko@mail.ru>
2022-12-08 19:04:37 +0300

    Вычислили выходные дни

---
+++ task16/дни недели.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число 1: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a<= 5)
+
+    Console.WriteLine("Нет");
+
+else
+
+{
+    Console.WriteLine("Да");
+}
+
+
+
